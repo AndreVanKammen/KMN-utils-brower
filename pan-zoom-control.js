4fbb881fc842faaa302cca5e9ada2bb27423f100
Andre van Kammen <andrevankammen@gmail.com>
2021-10-11 20:26:56 +0200

    PanZoomChild support

--- pan-zoom-control.js
+++ pan-zoom-control.js
@@ -39,39 +39,6 @@ export class PanZoomBase {
 
     /* @type {Array<ControlHandler>) */
     this.handlers = [];
-  }
-
-  /**
-   * Adds a control handler to be used for handling the controls
-   * @param {ControlHandler} controlHandler 
-   */
-  addHandler(controlHandler) {
-    // Last add 1st handle because it's on top
-    this.handlers.unshift(controlHandler)
-  }
-
-}
-
-class PanZoomControl extends PanZoomBase {
-  /**
-   * @param {HTMLElement} element 
-   * @param {*} options 
-   */
-  constructor(element, options) {
-    super();
-
-    this.element = element;
-    options = options || {};
-    this.options = { ...defaultOptions, ...options }
-
-    this.lastTime = 0.0;
-    this.zoomCenterX = 0.5;
-    this.zoomCenterY = 0.5;
-    this.isPanning = false;
-    this.easeFactor = 0.6;
-    this.zoomSpeed = 5.0;
-
-    this.leftScrollMargin = this.options.maxYScale === this.options.minYScale ? 0.0 : 32.0;
 
     this.onClick = (x, y) => {
       for (let h of this.handlers) {
@@ -122,10 +89,75 @@ class PanZoomControl extends PanZoomBase {
       return false;
     };
 
+  }
+
+  /**
+   * Adds a control handler to be used for handling the controls
+   * @param {ControlHandler} controlHandler 
+   */
+  addHandler(controlHandler) {
+    // Last add 1st handle because it's on top
+    this.handlers.unshift(controlHandler)
+  }
+
+}
+export class PanZoomChild extends PanZoomBase {
+  /**
+   * 
+   * @param {PanZoomBase} parent 
+   */
+  constructor(parent) {
+    super();
+    this.parent = parent;
+    this.myWidth = 1.0; // My width within parentWidth
+    this.myHeight = 1.0; // My height within parentHeight
+    this.myXOffset = 0.0; // My offset within parentWidth
+    this.myYOffset = 0.0; // My offset within parentHeight
+    this.parentWidth = 10.0;
+    this.parentHeight = 2.0;
+  }
+
+  update() {
+    let widthFactor = this.myWidth / this.parentWidth;
+    let heightFactor = this.myHeight / this.parentHeight;  // 2 /4 = 0.5
+    this.xOffset = this.parent.xOffsetSmooth / widthFactor- this.myXOffset / this.myWidth;// / this.parentWidth;
+    this.xScale = this.parent.xScaleSmooth * widthFactor;
+    this.yOffset = this.parent.yOffsetSmooth - this.myYOffset;
+    this.yScale = this.parent.yScaleSmooth * heightFactor;
+    this.xOffsetSmooth = this.xOffset;
+    this.xScaleSmooth = this.xScale;
+    this.yOffsetSmooth = this.yOffset;
+    this.yScaleSmooth = this.yScale;
+  }
+}
+
+export default class PanZoomControl extends PanZoomBase {
+  /**
+   * @param {HTMLElement} element 
+   * @param {*} options 
+   */
+  constructor(element, options) {
+    super();
+
+    this.element = element;
+    options = options || {};
+    this.options = { ...defaultOptions, ...options }
+
+    this.lastTime = 0.0;
+    this.zoomCenterX = 0.5;
+    this.zoomCenterY = 0.5;
+    this.isPanning = false;
+    this.easeFactor = 0.6;
+    this.zoomSpeed = 5.0;
+
+    this.leftScrollMargin = this.options.maxYScale === this.options.minYScale ? 0.0 : 32.0;
+
     this.clear();
 
     let mouseInside = false;
     let keyStillDown = false;
+
+    // TODO: Make global handler for everything
     window.addEventListener('keydown', (event) => {
       if (mouseInside) {
         this.event = event;
@@ -312,5 +344,3 @@ class PanZoomControl extends PanZoomBase {
     // TODO: unbind all events
   }
 }
-
-export default PanZoomControl;
